7bb1cf171912300e55e25810ce40f8cd24730575
andreyskorohod <andrey.skorohod@zaelab.com>
2016-09-04 15:08:29 +0300

    add php handler for registration form; styles for welcome page

---
+++ formSubmit.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome!</title>
+        <link rel="stylesheet" href="css/welcome.css">
+    </head>
+    <body>
+        <div class="welcome-page">
+            <span>Welcome <?php echo $_POST["email"]; ?></span>
+        </div>
+    </body>
+</html>
